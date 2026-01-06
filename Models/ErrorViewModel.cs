68ca1144db076ec94876ef10b13f3fb78dfa0318
TrungkevinIT <trung.dev@gmail.com>
2026-01-07 01:37:30 +0700

    khoi tao du an

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanGiayTheThao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
