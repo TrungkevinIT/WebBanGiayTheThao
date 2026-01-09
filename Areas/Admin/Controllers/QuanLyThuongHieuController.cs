5a70ca735944b47520f1f9897efc6fb9d262c142
toofphamne <0306231402@caothang.edu.vn>
2026-01-09 08:19:27 +0700

    them thuong hieu

--- Areas/Admin/Controllers/QuanLyThuongHieuController.cs
+++ Areas/Admin/Controllers/QuanLyThuongHieuController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebBanGiayTheThao.Data;
+using WebBanGiayTheThao.Models;
 
 namespace WebBanGiayTheThao.Areas.Admin.Controllers
 {
@@ -25,6 +26,38 @@ namespace WebBanGiayTheThao.Areas.Admin.Controllers
             var danhsach = await query.ToListAsync();
             ViewData["CurrentFilter"] = searchString;
             return View(danhsach);
+        }
+
+        [HttpGet]
+        public IActionResult TrangThemThuongHieu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TrangThemThuongHieu([Bind("TenThuongHieu,XuatXu,TrangThai")] ThuongHieu thuongHieu)
+        {
+            bool checkTonTai = await _context.ThuongHieus.AnyAsync(t => t.TenThuongHieu == thuongHieu.TenThuongHieu);
+            if (checkTonTai)
+            {
+                ModelState.AddModelError("TenThuongHieu", "Thương hiệu đã tồn tại!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(thuongHieu);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(TrangQLThuongHieu));
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Đã có lỗi xảy ra khi lưu dữ liệu.");
+                }
+            }
+            return View(thuongHieu);
         }
     }
 }
