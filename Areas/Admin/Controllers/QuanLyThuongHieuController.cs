38ba5a94b240d40a25f07d777e74c7262490141f
toofphamne <0306231402@caothang.edu.vn>
2026-01-09 07:56:44 +0700

    load danh sach

--- Areas/Admin/Controllers/QuanLyThuongHieuController.cs
+++ Areas/Admin/Controllers/QuanLyThuongHieuController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiayTheThao.Data;
 
 namespace WebBanGiayTheThao.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class QuanLyThuongHieuController : Controller
     {
-        public IActionResult TrangQLThuongHieu()
+        private readonly QuanLyWebBanGiayContext _context;
+        public QuanLyThuongHieuController(QuanLyWebBanGiayContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> TrangQLThuongHieu()
+        {
+            var danhsach = await _context.ThuongHieus.ToListAsync();
+            return View(danhsach);
         }
     }
 }
