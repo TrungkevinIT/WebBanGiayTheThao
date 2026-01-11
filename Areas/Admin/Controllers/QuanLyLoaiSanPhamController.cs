5a38be3655393f8ac439a71183aabff736460692
khoinguyen1309 <0306231410@caothang.edu.vn>
2026-01-11 22:18:41 +0700

    tao cac giao dien quan ly loai san pham

--- Areas/Admin/Controllers/QuanLyLoaiSanPhamController.cs
+++ Areas/Admin/Controllers/QuanLyLoaiSanPhamController.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebBanGiayTheThao.Filters;
 
 namespace WebBanGiayTheThao.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [AdminAuthorize]
-
     public class QuanLyLoaiSanPhamController : Controller
     {
         public IActionResult TrangQLLoaiSanPham()
+        {
+            return View();
+        }
+        public IActionResult TrangThemMoiLoaiSanPham()
+        {
+            return View();
+        }
+        public IActionResult TrangCapNhatLoaiSanPham()
         {
             return View();
         }
