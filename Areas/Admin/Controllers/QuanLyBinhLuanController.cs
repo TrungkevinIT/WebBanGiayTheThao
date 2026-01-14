07f92c30e9d0eb80163f44da3fdfc3aac3c35826
tranduykhang0306231390 <0306231390@caothang.edu.vn>
2026-01-14 14:16:29 +0700

    Thêm chức năng quản lí bình luận cho Admin

---
+++ Areas/Admin/Controllers/QuanLyBinhLuanController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiayTheThao.Data;
+using WebBanGiayTheThao.Filters;
+
+namespace WebBanGiayTheThao.Controllers.Admin
+{
+    [Area("Admin")]
+    [AdminAuthorize]
+    public class QuanLyBinhLuanController : Controller
+    {
+        private readonly QuanLyWebBanGiayContext _context;
+
+        public QuanLyBinhLuanController(QuanLyWebBanGiayContext context)
+        {
+            _context = context;
+        }
+
+        // ================= DANH SÁCH + LỌC =================
+        public IActionResult TrangQuanLyBinhLuan(int? trangThai)
+        {
+            var query = _context.BinhLuans
+                .Include(x => x.User)
+                .Include(x => x.SanPham)
+                .OrderByDescending(x => x.NgayTao)
+                .AsQueryable();
+
+            if (trangThai.HasValue)
+            {
+                query = query.Where(x => x.TrangThai == trangThai);
+            }
+
+            ViewBag.TrangThai = trangThai;
+            return View(query.ToList());
+        }
+
+        // ================= DUYỆT =================
+        [HttpPost]
+        public IActionResult Duyet(int id)
+        {
+            var bl = _context.BinhLuans.Find(id);
+            if (bl == null) return NotFound();
+
+            bl.TrangThai = 1;
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(TrangQuanLyBinhLuan));
+        }
+
+        // ================= ẨN =================
+        [HttpPost]
+        public IActionResult An(int id)
+        {
+            var bl = _context.BinhLuans.Find(id);
+            if (bl == null) return NotFound();
+
+            bl.TrangThai = 2;
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(TrangQuanLyBinhLuan));
+        }
+    }
+}
