39a8b531f02b6eca22d0bd8594177703ef42d923
khoinguyen1309 <0306231410@caothang.edu.vn>
2026-01-14 23:52:10 +0700

    tạo viewcomponent cho sản phẩm liên quan

---
+++ ViewComponents/BinhLuanComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiayTheThao.Data;
+
+namespace WebBanGiayTheThao.ViewComponents
+{
+    public class BinhLuanComponent: ViewComponent
+    {
+        private readonly QuanLyWebBanGiayContext _context;
+        public BinhLuanComponent(QuanLyWebBanGiayContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int loaiSpId, int sanPhamId)
+        {
+            var sp = await _context.SanPhams
+                .Where(x => x.LoaiSanPhamId == loaiSpId && x.Id != sanPhamId)
+                .OrderBy(r => Guid.NewGuid()) // Random ngẫu nhiên để tạo cảm giác đa dạng
+                .Take(4)
+                .ToListAsync();
+
+            return View(sp);
+        }
+    }
+}
---
+++ ViewComponents/SanPhamLienQuanComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiayTheThao.Data;
+using WebBanGiayTheThao.Models;
+
+namespace WebBanGiayTheThao.ViewComponents
+{
+    public class SanPhamLienQuanComponent : ViewComponent
+    {
+        private readonly QuanLyWebBanGiayContext _context;
+        public SanPhamLienQuanComponent(QuanLyWebBanGiayContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int loaiSpId, int sanPhamId)
+        {
+            var sp = await _context.SanPhams
+                .Where(x => x.LoaiSanPhamId == loaiSpId && x.Id != sanPhamId)
+                .OrderBy(r => Guid.NewGuid()) // Random ngẫu nhiên để tạo cảm giác đa dạng
+                .Take(4)
+                .ToListAsync();
+
+            return View(sp);
+        }
+
+        
+    }
+}
