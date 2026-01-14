a606514309533c69ca8304438f69fb32f1dd8530
tranduykhang0306231390 <0306231390@caothang.edu.vn>
2026-01-14 14:39:46 +0700

    Update footer component hiển thị websetting

---
+++ ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBanGiayTheThao.Data;
+using System.Linq;
+
+namespace WebBanGiayTheThao.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly QuanLyWebBanGiayContext _context;
+
+        public FooterViewComponent(QuanLyWebBanGiayContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var setting = _context.WebSettings.FirstOrDefault();
+            return View(setting);
+        }
+    }
+}
