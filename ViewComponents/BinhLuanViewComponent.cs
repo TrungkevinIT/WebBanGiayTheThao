03c355992b50ee82a312c1549e0634942c6764f1
khoinguyen1309 <0306231410@caothang.edu.vn>
2026-01-15 23:09:13 +0700

    cập nhật phân trang cho bình luận

--- ViewComponents/BinhLuanViewComponent.cs
+++ ViewComponents/BinhLuanViewComponent.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebBanGiayTheThao.Data;
+using WebBanGiayTheThao.ViewModels.BinhLuan;
 
 namespace WebBanGiayTheThao.ViewComponents
 {
@@ -13,14 +14,29 @@ namespace WebBanGiayTheThao.ViewComponents
             _context = context;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(int spId)
+        public async Task<IViewComponentResult> InvokeAsync(int spId,int trang=1)
         {
-            var danhGia = await _context.BinhLuans
+            var query = _context.BinhLuans
                 .Include(x => x.User)
-                .Where(x => x.SanPhamId== spId) 
+                .Where(x => x.SanPhamId == spId);
+            int tongComment = await query.CountAsync();
+            // Tính điểm đánh giá trung bình
+            double diemTrungBinh = tongComment > 0 ? (await query.AverageAsync(x => x.Sao)) ?? 0 : 0;
+            var pagedComments = await query
                 .OrderByDescending(x => x.NgayTao)
+                .Skip((trang - 1) * 5)
+                .Take(5)
                 .ToListAsync();
-            return View("BinhLuanSanPham", danhGia);
+            var model = new BinhLuanVM
+            {
+                BinhLuan = pagedComments,
+                TongBinhLuan = tongComment,
+                TrungBinhDanhGia = Math.Round(diemTrungBinh, 1),
+                TrangHienTai = trang,
+                TongSoTrang = (int)Math.Ceiling(tongComment / 5.0)
+            };
+                
+            return View("BinhLuanSanPham", model);
         }
     }
 }
