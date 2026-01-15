a7fca867f40411c9b8fdad149eb1269047d17fd3
khoinguyen1309 <0306231410@caothang.edu.vn>
2026-01-15 22:21:13 +0700

    hiển thị bình luận của sản phẩm, cập nhật lại viewcomponent BL

---
+++ ViewComponents/BinhLuanViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiayTheThao.Data;
+
+namespace WebBanGiayTheThao.ViewComponents
+{
+    public class BinhLuanViewComponent : ViewComponent
+    {
+        private readonly QuanLyWebBanGiayContext _context;
+
+        public BinhLuanViewComponent(QuanLyWebBanGiayContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int spId)
+        {
+            var danhGia = await _context.BinhLuans
+                .Include(x => x.User)
+                .Where(x => x.SanPhamId== spId) 
+                .OrderByDescending(x => x.NgayTao)
+                .ToListAsync();
+            return View("BinhLuanSanPham", danhGia);
+        }
+    }
+}
