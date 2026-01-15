a3ca8287cc5cf6a27b533771c94afc6bcbe12758
TrungkevinIT <trungquocdo2005@gmail.com>
2026-01-15 22:42:39 +0700

    chuc nang danh muc noi bat

---
+++ ViewComponents/LoaiSanPhamViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiayTheThao.Data;
+using WebBanGiayTheThao.Services;
+
+namespace WebBanGiayTheThao.ViewComponents
+{
+    public class LoaiSanPhamViewComponent : ViewComponent
+    {
+        private readonly ILoaiSanPhamService _loaiSanPhamService;
+        public LoaiSanPhamViewComponent(ILoaiSanPhamService loaiSanPhamService)
+        {
+            _loaiSanPhamService = loaiSanPhamService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var dsloaisp = await _loaiSanPhamService.GetDanhMucNoiBatAsync();
+            return View("TrangLoaiSanPham",dsloaisp);
+        }
+    }
+}
