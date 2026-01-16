686b073bd65f9f0488631cb7b6d839bf65b81d79
TrungkevinIT <trungquocdo2005@gmail.com>
2026-01-16 19:12:17 +0700

    load danh sach sp o trang sp

--- Controllers/SanPhamController.cs
+++ Controllers/SanPhamController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebBanGiayTheThao.Filters;
+using WebBanGiayTheThao.Services;
 
 namespace WebBanGiayTheThao.Controllers
 {
     public class SanPhamController : Controller
     {
-        public IActionResult TrangSanPham()
+        private readonly ISanPhamService _sp;
+        public SanPhamController(ISanPhamService sp)
         {
-            return View();
+            _sp = sp;
+        }
+        public async Task<IActionResult> TrangSanPham(string? search, int? loaispid, int? thuonghieuid, string? gia, int page=1)
+        {
+            int pageSize = 8;
+            var item = await _sp.LoadDSSanPham(search, loaispid, thuonghieuid, gia,1, page, pageSize);
+            return View(item);
         }
         public IActionResult TrangChiTietSanPham(int id)
         {
